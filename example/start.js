3fabf8bc8290f86796bff044542d4768a8381972
Dan B <blueseph@gmail.com>
2017-07-25 18:54:51 -0700

    updated travis to work with object spread operator

--- example/start.js
+++ example/start.js
@@ -12,9 +12,9 @@ app.middlewares.add(middleware1);
 
 app.resource({ tableName: 'rooms' });
 
-//app.resources.rooms.validates('owner_id', room => room.owner_id);
-//app.resources.rooms.validates('description', room => room.description);
-//app.resources.rooms.validates('description', room => room.description.length > 5, 'Descriptions must be at least five characters long');
+app.resources.rooms.validates('owner_id', room => room.owner_id);
+app.resources.rooms.validates('description', room => room.description);
+app.resources.rooms.validates('description', room => room.description.length > 5, 'Descriptions must be at least five characters long');
 
 app.resources.rooms.middlewares.add(middleware2);
 
